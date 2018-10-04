6da9d19dc11c10269e618a438395ae649000411c
Aamina Munzir <aaminamunzir@aaminas-macbook-air.local>
2018-10-04 12:40:17 -0500

    first table view is list of amino series

--- APIProject/SourcesViewController.swift
+++ APIProject/SourcesViewController.swift
@@ -71,5 +71,27 @@ class SourcesViewController: UITableViewController {
     
     
     
+    
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return amiibo.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        let source = amiibo[indexPath.row]
+        cell.textLabel?.text = source["name"]
+        cell.detailTextLabel?.text = source["key"]
+        
+        return cell
+    }
+    
+    
+    
+    
+    
+    
 }
 
