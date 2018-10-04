59d8f44f2fe1f174f4f4cb01a84a0210a0bf8ffd
Aamina Munzir <aaminamunzir@aaminas-macbook-air.local>
2018-10-04 12:33:21 -0500

    LET APPLE SECURITY B U R N

--- APIProject/SourcesViewController.swift
+++ APIProject/SourcesViewController.swift
@@ -10,11 +10,66 @@ import UIKit
 
 class SourcesViewController: UITableViewController {
 
+    var amiibo = [[String:String]]()
+    let apiKey = ""
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.title = "Amiibo Series"
+        let query = "http://www.amiiboapi.com/api/amiiboseries/"
+        
+        print("something's happening")
+        
+        if let url = URL(string: query)
+        {
+            print("passed url bit")
+            if let data = try? Data(contentsOf: url)
+            {
+                print("passed data bit")
+                let json = try! JSON(data: data)
+                /*if json["status"] == "ok"
+                {
+                    print("passed status bit")
+                    parse(json: json)
+                    return
+                }*/
+                
+                parse(json: json)
+                return
+            }
+        }
+        loadError()
+        
     }
 
 
+    func parse(json: JSON)
+    {
+        for result in json["amiibo"].arrayValue
+        {
+            let name = result["name"].stringValue
+            let key = result["key"].stringValue
+            
+            let amiiboo = ["name":name, "key": key]
+            amiibo.append(amiiboo)
+        }
+        tableView.reloadData()
+    }
+    
+    
+    func loadError() {
+        let alert = UIAlertController(title: "Loading Error", message: "There was a problem loading the news feed", preferredStyle: .actionSheet)
+        
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
+    
+    
 }
 
