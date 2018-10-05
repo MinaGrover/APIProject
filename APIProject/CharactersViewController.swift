dfba5d6b4a26f0b33dedd4493f60c2f4366a2309
Aamina Munzir <aaminamunzir@aaminas-macbook-air.local>
2018-10-05 11:35:49 -0500

    added chars view controller

---
+++ APIProject/CharactersViewController.swift
@@ -0,0 +1,109 @@
+//
+//  ViewController.swift
+//  APIProject
+//
+//  Created by Aamina Munzir on 9/28/18.
+//  Copyright © 2018 Aamina Munzir. All rights reserved.
+//
+
+import UIKit
+
+class CharactersViewController: UITableViewController {
+    
+    var chars = [[String:String]]()
+    var apiKey = ""
+    var amiiboType = [String: String]()
+    
+    
+    var name = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.title = "Characters in \(amiiboType["name"]!)"
+        let query = "http://www.amiiboapi.com/api/amiibo/?amiiboSeries=\(amiiboType["name"]!)"
+        
+        print("something's happening")
+        
+        DispatchQueue.global(qos: .userInitiated).async {
+            [unowned self] in
+            if let url = URL(string: query)
+            {
+                print("passed url bit")
+                if let data = try? Data(contentsOf: url)
+                {
+                    print("passed data bit")
+                    let json = try! JSON(data: data)
+                    
+                    
+                    self.parse(json: json)
+                    return
+                }
+            }
+            self.loadError()
+        }
+    }
+    
+    
+    func parse(json: JSON)
+    {
+        for result in json["amiiboType"].arrayValue
+        {
+            let name = result["name"].stringValue
+            let gameSeries = result["gameSeries"].stringValue
+            let series = result["amiiboSeries"].stringValue
+            let imageURL = result["image"].stringValue
+            
+            let amiiboo = ["name":name, "gameSeries": gameSeries, "amiiboSeries": series, "imageURL": imageURL]
+            chars.append(amiiboo)
+        }
+        
+        DispatchQueue.main.async {
+            [unowned self] in
+            self.tableView.reloadData()
+        }
+        
+    }
+    
+    
+    func loadError() {
+        
+        DispatchQueue.main.async {
+            [unowned self] in
+            
+            let alert = UIAlertController(title: "Loading Error", message: "There was a problem loading the news feed", preferredStyle: .actionSheet)
+            
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            
+            self.present(alert, animated: true, completion: nil)
+        }
+        
+    }
+    
+    
+    
+    
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return chars.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        let char = chars[indexPath.row]
+        cell.textLabel?.text = char["name"]
+        cell.detailTextLabel?.text = char["gameSeries"]
+        
+        return cell
+    }
+    
+    
+    
+    
+    
+    
+}
+
